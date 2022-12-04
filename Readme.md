db1dd247b0d35f0b3444b9c162dbcb97391aabe0
Olga <olga.myazina@outlook.com>
2022-12-04 23:44:04 +0300

    feat: solution to subtask cp

--- Readme.md
+++ Readme.md
@@ -225,3 +225,21 @@ To check `wt`, you need to execute
 ```
 npm run wt
 ```
+
+### Child Processes
+
+---
+
+### cp
+
+`cp.js` - implement function `spawnChildProcess` that receives array of arguments args and creates child process from file `script.js`, passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
+- child process `stdin` should receive input from master process `stdin`
+- child process `stdout` should send data to master process `stdout`
+
+#### How to check
+
+To check `cp` to execute
+
+```
+npm run cp
+```
--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process'
+import {stdin, stdout, execPath} from 'node:process'
+import {resolve} from 'node:path'
+
+const PATH_TO_FILE = 'src/cp/files/script.js'
+
+const spawnChildProcess = async (...args) => {
+    console.log({args})
+    const pathToScript = resolve(PATH_TO_FILE)
+    const childProcess = spawn(execPath, [pathToScript, ...args]);
+    stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(stdout);
+
 };
 
-spawnChildProcess();
+await spawnChildProcess('elem1', 'elem2');
+
